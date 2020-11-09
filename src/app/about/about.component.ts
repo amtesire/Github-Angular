78b6845d04335b0d258a54005f739c2db316c49c
amtesire <tesiregisele@gmail.com>
2020-11-09 19:04:59 +0200

    added compoments

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import{ProfileService} from '../profile-service/profile.service'
 
 @Component({
   selector: 'app-about',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  profile:any;
+  repos:any;
+   username:any;
 
-  constructor() { }
+  constructor(public profileService:ProfileService) {
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile=profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos=repos;
+    });
+  }
+  findProfile(){
+    this.profileService.updateProfile(this.username);
+    this.profileService.getProfileInfo().subscribe (profile => {
+      console.log(profile);
+      this.profile=profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos=repos;
+    });
+  }
 
-  ngOnInit(): void {
+  
+
+  ngOnInit() {
   }
 
 }
