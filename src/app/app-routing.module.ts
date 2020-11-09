aa2bfcc3053d433c512118c0f3a16f06ad9646f3
amtesire <tesiregisele@gmail.com>
2020-11-09 17:28:27 +0200

    added some imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { AboutComponent } from './about/about.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'github-profile', component: GithubProfileComponent},
+  { path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
