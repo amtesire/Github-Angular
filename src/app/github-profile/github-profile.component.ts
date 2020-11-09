457e45bba40a629546f83db2c4162ce7f5bffc0b
amtesire <tesiregisele@gmail.com>
2020-11-09 19:08:51 +0200

    added some elements

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../user';
+import { HttpClient } from '@angular/common/http';
+import { ProfileService } from '../profile-service/profile.service';
+//import {Repository} '../repository-class/repository';
 
 @Component({
   selector: 'app-github-profile',
   templateUrl: './github-profile.component.html',
+  providers:[ProfileService],
   styleUrls: ['./github-profile.component.css']
 })
 export class GithubProfileComponent implements OnInit {
+ 
+ 
+ public username='amtesire';
+ public  getProfileInfo:string;
+ user:User;
+//  findUser(username){
+//    this. getProfileInfo='';
+//    this.username=username;
+//    this.ngOnInit();
+//  }
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  constructor(public userRequest:ProfileService,public repoRequest:ProfileService) { }
+
+  ngOnInit() {
+    // this.userRequest.users(this.username);
+    // this.user=this.userRequest.user;
+    //this.repoRequest.use
+  
   }
 
 }
